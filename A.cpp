c2de6a9dd3f3b288f797629091e903e38b952266
OnjoujiToki <z@moon.moe>
2022-12-07 12:18:35 -0800

    init repo

---
+++ A.cpp
@@ -0,0 +1,186 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+int mod;
+template <typename T>
+struct SegmentTree {
+  using Monoid = typename T::Monoid;
+
+  explicit SegmentTree(int n) : SegmentTree(std::vector<Monoid>(n, T::id())) {}
+
+  explicit SegmentTree(const std::vector<Monoid> &a) : n(a.size()), p2(1) {
+    while (p2 < n) p2 <<= 1;
+    dat.assign(p2 << 1, T::id());
+    std::copy(a.begin(), a.end(), dat.begin() + p2);
+    for (int i = p2 - 1; i > 0; --i) {
+      dat[i] = T::merge(dat[i << 1], dat[(i << 1) + 1]);
+    }
+  }
+
+  void set(int idx, const Monoid val) {
+    idx += p2;
+    dat[idx] = val;
+    while (idx >>= 1) dat[idx] = T::merge(dat[idx << 1], dat[(idx << 1) + 1]);
+  }
+
+  Monoid get(int left, int right) const {
+    Monoid res_l = T::id(), res_r = T::id();
+    for (left += p2, right += p2; left < right; left >>= 1, right >>= 1) {
+      if (left & 1) res_l = T::merge(res_l, dat[left++]);
+      if (right & 1) res_r = T::merge(dat[--right], res_r);
+    }
+    return T::merge(res_l, res_r);
+  }
+
+  Monoid operator[](const int idx) const { return dat[idx + p2]; }
+
+  template <typename G>
+  int find_right(int left, const G g) {
+    if (left >= n) return n;
+    Monoid val = T::id();
+    left += p2;
+    do {
+      while (!(left & 1)) left >>= 1;
+      Monoid nxt = T::merge(val, dat[left]);
+      if (!g(nxt)) {
+        while (left < p2) {
+          left <<= 1;
+          nxt = T::merge(val, dat[left]);
+          if (g(nxt)) {
+            val = nxt;
+            ++left;
+          }
+        }
+        return left - p2;
+      }
+      val = nxt;
+      ++left;
+    } while (__builtin_popcount(left) > 1);
+    return n;
+  }
+
+  template <typename G>
+  int find_left(int right, const G g) {
+    if (right <= 0) return -1;
+    Monoid val = T::id();
+    right += p2;
+    do {
+      --right;
+      while (right > 1 && (right & 1)) right >>= 1;
+      Monoid nxt = T::merge(dat[right], val);
+      if (!g(nxt)) {
+        while (right < p2) {
+          right = (right << 1) + 1;
+          nxt = T::merge(dat[right], val);
+          if (g(nxt)) {
+            val = nxt;
+            --right;
+          }
+        }
+        return right - p2;
+      }
+      val = nxt;
+    } while (__builtin_popcount(right) > 1);
+    return -1;
+  }
+
+  int MOD;
+  const int n;
+  int p2;
+  std::vector<Monoid> dat;
+};
+
+namespace monoid {
+
+template <typename T>
+struct RangeMinimumQuery {
+  using Monoid = T;
+  static constexpr Monoid id() { return std::numeric_limits<Monoid>::max(); }
+  static Monoid merge(const Monoid &a, const Monoid &b) {
+    return std::min(a, b);
+  }
+};
+
+template <typename T>
+struct RangeMaximumQuery {
+  using Monoid = T;
+  static constexpr Monoid id() { return std::numeric_limits<Monoid>::lowest(); }
+  static Monoid merge(const Monoid &a, const Monoid &b) {
+    return std::max(a, b);
+  }
+};
+
+template <typename T>
+struct RangeSumQuery {
+  using Monoid = T;
+  static constexpr Monoid id() { return 0; }
+  static Monoid merge(const Monoid &a, const Monoid &b) { return a + b; }
+};
+
+template <typename T>
+struct RangeXorQuery {
+  using Monoid = T;
+  static constexpr Monoid id() { return 0; }
+  static Monoid merge(const Monoid &a, const Monoid &b) { return a ^ b; }
+};
+
+template <typename T>
+struct RangeGCDQuery {
+  using Monoid = T;
+  static constexpr Monoid id() { return 0; }
+  static Monoid merge(const Monoid &a, const Monoid &b) {
+    return std::gcd(a, b);
+  }
+};
+
+}  // namespace monoid
+// g++ -std=c++17 A.cpp -o A  && timeout 4s ./A<input.in>output.out
+// https://github.com/online-judge-tools/verification-helper/blob/master/README.md
+void solve() {
+  int n;
+  std::cin >> n >> mod;
+
+  struct RangeProductQuery {
+    using Monoid = long long;
+    static constexpr long long id() { return 1LL; }
+    static long long merge(const long long &a, const long long &b) {
+      return a * b % mod;
+    }
+  };
+  
+  SegmentTree<RangeProductQuery> tr(n + 1);
+  for (int i = 1; i <= n; i++) {
+    int op, m;
+    std::cin >> op >> m;
+    if (op == 1) {
+      tr.set(i, m);
+    } else {
+      tr.set(m, 1);
+    }
+    std::cout << tr.get(1, n + 1) << '\n';
+  }
+}
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  int t;
+  std::cin >> t;
+  while (t--) solve();
+  return 0;
+}
