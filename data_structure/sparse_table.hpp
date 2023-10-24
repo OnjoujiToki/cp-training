9585c9bf7782c27d6bd1e06b04875364827c8f47
OnjoujiToki <z@moon.moe>
2023-10-24 07:16:23 -0400

    add sparse table

---
+++ data_structure/sparse_table.hpp
@@ -0,0 +1,43 @@
+template <typename Band>
+struct SparseTable {
+  using BinOp = std::function<Band(Band, Band)>;
+
+  SparseTable() = default;
+
+  explicit SparseTable(const std::vector<Band>& a, const BinOp bin_op) {
+    init(a, bin_op);
+  }
+
+  void init(const std::vector<Band>& a, const BinOp bin_op_) {
+    bin_op = bin_op_;
+    const int n = a.size();
+    assert(n > 0);
+    lg.assign(n + 1, 0);
+    for (int i = 2; i <= n; ++i) {
+      lg[i] = lg[i >> 1] + 1;
+    }
+    const int table_h = std::countr_zero(std::bit_floor(a.size())) + 1;
+    data.assign(table_h, std::vector<Band>(n));
+    std::copy(a.begin(), a.end(), data.front().begin());
+    for (int i = 1; i < table_h; ++i) {
+      for (int j = 0; j + (1 << i) <= n; ++j) {
+        data[i][j] = bin_op(data[i - 1][j], data[i - 1][j + (1 << (i - 1))]);
+      }
+    }
+  }
+
+  Band query(const int left, const int right) const {
+    assert(left < right);
+    const int h = lg[right - left];
+    return bin_op(data[h][left], data[h][right - (1 << h)]);
+  }
+
+ private:
+  BinOp bin_op;
+  std::vector<int> lg;
+  std::vector<std::vector<Band>> data;
+};
+
+SparseTable<int> dst(a, [](const int a, const int b) -> int {
+  return std::min(a, b);
+});
