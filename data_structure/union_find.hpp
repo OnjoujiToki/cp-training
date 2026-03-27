159d6d0c63d8c1f1fd82ef8e3fbaaa810abce69a
Ren Ai <z@moon.moe>
2026-03-26 17:29:06 -0700

    update: refactor WeightedUnionFind to WeightedDSU with improved structure and functionality

--- data_structure/union_find.hpp
+++ data_structure/union_find.hpp
@@ -1,21 +1,65 @@
-template <typename T>
-struct DSU {
-  std::vector<T> f, siz;
-  DSU(int n) : f(n), siz(n, 1) { std::iota(f.begin(), f.end(), 0); }
-  T leader(T x) {
-    while (x != f[x]) x = f[x] = f[f[x]];
+struct dsu {
+ public:
+  dsu() : _n(0) {}
+  explicit dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+  int merge(int a, int b) {
+    assert(0 <= a && a < _n);
+    assert(0 <= b && b < _n);
+    int x = leader(a), y = leader(b);
+    if (x == y) return x;
+    if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+    parent_or_size[x] += parent_or_size[y];
+    parent_or_size[y] = x;
     return x;
   }
-  bool same(T x, T y) { return leader(x) == leader(y); }
-  bool merge(T x, T y) {
-    x = leader(x);
-    y = leader(y);
-    if (x == y) return false;
-    siz[x] += siz[y];
-    f[y] = x;
-    return true;
+
+  bool same(int a, int b) {
+    assert(0 <= a && a < _n);
+    assert(0 <= b && b < _n);
+    return leader(a) == leader(b);
   }
-  T size(int x) { return siz[leader(x)]; }
+
+  int leader(int a) {
+    assert(0 <= a && a < _n);
+    return _leader(a);
+  }
+
+  int size(int a) {
+    assert(0 <= a && a < _n);
+    return -parent_or_size[leader(a)];
+  }
+
+  std::vector<std::vector<int>> groups() {
+    std::vector<int> leader_buf(_n), group_size(_n);
+    for (int i = 0; i < _n; i++) {
+      leader_buf[i] = leader(i);
+      group_size[leader_buf[i]]++;
+    }
+    std::vector<std::vector<int>> result(_n);
+    for (int i = 0; i < _n; i++) {
+      result[i].reserve(group_size[i]);
+    }
+    for (int i = 0; i < _n; i++) {
+      result[leader_buf[i]].push_back(i);
+    }
+    result.erase(
+        std::remove_if(result.begin(), result.end(),
+                       [&](const std::vector<int>& v) { return v.empty(); }),
+        result.end());
+    return result;
+  }
+
+ private:
+  int _n;
+  // root node: -1 * component size
+  // otherwise: parent
+  std::vector<int> parent_or_size;
+
+  int _leader(int a) {
+    if (parent_or_size[a] < 0) return a;
+    return parent_or_size[a] = _leader(parent_or_size[a]);
+  }
 };
 
 struct DSU {
